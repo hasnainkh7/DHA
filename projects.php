5507e8a7c4d8a6053ebd7c54fb1316a2cfa68759
Hasnain Khan <hashukh7@gmail.com>
2022-02-22 17:20:22 +0500

    LastUpdate

---
+++ projects.php
@@ -0,0 +1,82 @@
+<?php
+$curPpageTitle = "All Projects"; 
+include("inc/header.php");
+?>
+
+
+
+<div class="row">
+    <div class="col-md-12 mb-3">
+        <h2 class="text-black font-w600 fs-32">DHA</h2>
+    </div>
+
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-sm-3">
+                <a href="projectdata.php">
+                <div class="card avtivity-card">
+                    <div class="card-body">
+                        <div class="media align-items-center">
+                            <div class="media-body text-center">
+                                <span class="title text-black font-w600">Islamabad</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="effect bg-success"></div>
+                </div>
+                </a>
+            </div>
+
+            <div class="col-sm-3">
+                <a href="projectdata.php">
+                <div class="card avtivity-card">
+                    <div class="card-body">
+                        <div class="media align-items-center">
+                            <div class="media-body text-center">
+                                <span class="title text-black font-w600">Lahore</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="effect bg-secondary"></div>
+                </div>
+                </a>
+            </div>
+
+            <div class="col-sm-3">
+                <a href="projectdata.php">
+                <div class="card avtivity-card">
+                    <div class="card-body">
+                        <div class="media align-items-center">
+                            <div class="media-body text-center">
+                                <span class="title text-black font-w600">Karachi</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="effect bg-info"></div>
+                </div>
+                </a>
+            </div>
+
+            <div class="col-sm-3">
+                <a href="projectdata.php">
+                <div class="card avtivity-card">
+                    <div class="card-body">
+                        <div class="media align-items-center">
+                            <div class="media-body text-center">
+                                <span class="title text-black font-w600">Gujranwala</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="effect bg-success"></div>
+                </div>
+                </a>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+</div>
+<?php include("inc/footer.php");?>
